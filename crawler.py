ab855b67842be2731441ccc90a19969a3f5410d8
dmitry.sitnikov <dmitry.sitnikov@corp.mail.ru>
2016-10-24 16:24:39 +0300

    added file pattern

--- crawler.py
+++ crawler.py
@@ -9,7 +9,9 @@ class RTMPLink(scrapy.Item):
 class RTMPCrawlerSpider(scrapy.Spider):
     name = 'rtmp_crawler'
     start_urls = ['http://www.glaz.tv']
-    rtmp_link_pattern = re.compile('rtmp://[a-z0-9:/?%#.]*')
+    rtmp_link_pattern = re.compile('rtmp://[A-Za-z0-9:/?%#.\-_]*')
+    file_link_pattern = re.compile('file=[A-Za-z0-9:/?%#.\-_]*')
+
     links = set()
     filter_dictionary = set()
     filter_links = urlparse(start_urls[0]).netloc
@@ -19,7 +21,8 @@ class RTMPCrawlerSpider(scrapy.Spider):
 
         for rtmp in sel.re(self.rtmp_link_pattern):
             yield RTMPLink(link=rtmp, from_url=response.url)
-
+        for file_link in sel.re(self.file_link_pattern):
+            yield RTMPLink(link=file_link, from_url=response.url)
         for url in response.xpath('//a/@href').extract():
             # print url
             created = False
