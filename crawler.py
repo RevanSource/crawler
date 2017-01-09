ce9d7de73c335996af3a8fae78715aee86c83cc8
Dmitry <sitnikov_itip@mail.ru>
2017-01-09 21:33:50 +0300

    Update crawler.py
    clean up code

--- crawler.py
+++ crawler.py
@@ -8,15 +8,14 @@ class RTMPLink(scrapy.Item):
 
 class RTMPCrawlerSpider(scrapy.Spider):
     name = 'rtmp_crawler'
-    start_urls = ['http://www.glaz.tv']
+    start_url = 'http://www.glaz.tv'
     rtmp_link_pattern = re.compile('rtmp://[A-Za-z0-9:/?%#.\-_]*')
     file_link_pattern = re.compile('file=[A-Za-z0-9:/?%#.\-_]*')
 
     links = set()
     filter_dictionary = set()
-    filter_links = urlparse(start_urls[0]).netloc
+    filter_links = urlparse(start_url).netloc
     def parse(self, response):
-        # for rtmp in response.xpath('//a[contains(@href, "rtmp")]/@href').extract():
         sel = scrapy.Selector(response=response)
 
         for rtmp in sel.re(self.rtmp_link_pattern):
@@ -27,7 +26,7 @@ class RTMPCrawlerSpider(scrapy.Spider):
             # print url
             created = False
             if url[0] == '/':
-                url = self.start_urls[0] + url.strip()
+                url = self.start_url + url.strip()
                 created = True
                 # print "Parsed url" + url
             isOk = True
